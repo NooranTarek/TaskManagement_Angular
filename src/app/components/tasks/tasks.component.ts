43fa56da7f8ff9ded55b8ad40ab0abc3dcfc9758
Nouran Tarek <noort.mohamed@gmail.com>
2024-11-25 18:13:52 +0200

    adding sidebar in tasks page

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Task } from '../../interfaces/task';
 import { TaskService } from '../../services/task.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
