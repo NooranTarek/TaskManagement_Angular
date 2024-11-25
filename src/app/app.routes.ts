6a4bf1c9d0c0e558e617ee92468ec90aefb767bd
Nouran Tarek <noort.mohamed@gmail.com>
2024-11-25 10:45:42 +0200

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +14,12 @@ export const routes: Routes = [
         path:"tasks",
         component:TasksComponent
     },
-
+    {
+        path:"register",
+        component:SignUpComponent
+    },
+    {
+        path:"login",
+        component:SignInComponent
+    }
 ];
