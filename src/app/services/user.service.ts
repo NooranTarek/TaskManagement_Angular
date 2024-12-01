9fc52a5aa092190d39d434121fe12a2300c4265e
Nouran Tarek <noort.mohamed@gmail.com>
2024-12-01 16:39:08 +0200

    getting username and update style

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  private urlApi = 'http://localhost:8080/users';
+  getUserName():Observable<any>{
+    return this.http.get<any>(`${this.urlApi}`)
+  }
 }
