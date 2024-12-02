d4b56ff43b9866b9fd9d82312ce9681cb268058a
Nouran Tarek <noort.mohamed@gmail.com>
2024-12-02 16:27:25 +0200

    catching role from token

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import {jwtDecode} from 'jwt-decode';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token =localStorage.getItem('Authorization');
@@ -9,10 +10,20 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   
   const router = inject(Router); 
   if (token) {
+    try {
+      const decodedToken: any = jwtDecode(token);
+      // console.log('Decoded Token:', decodedToken);
+
+      const userRole = decodedToken.role;
+      // console.log('User Role:', userRole);
+    } catch (error) {
+      console.error('Error decoding token:', error);
+    }
     req = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
   }
+  
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
